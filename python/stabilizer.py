17ede1f79af30374083f73d28f7be89b9e5fd62d
Henrik Skov Midtiby <hemi@mmmi.sdu.dk>
2019-03-21 14:02:58 +0100

    Changing the structure of the video stabilizer. Now it automatically locates points to track and thus no initialization is needed.

--- python/stabilizer.py
+++ python/stabilizer.py
@@ -13,67 +13,53 @@ from cv_bridge import CvBridge, CvBridgeError
 
 
 class VideoStabilizer():
-    def __init__(self, crop_locations):
-        pass
+    def __init__(self):
         self.has_seen_first_frame = False
-        self.crop_locations = crop_locations
-        self.crop_height = 40
-        self.crop_width = 40
-        self.crops = []
-        self.crop_matches = []
 
+        # Initiate STAR detector
+        self.orb = cv2.ORB_create()
+
+        # Initiate FAST object with default values
+        self.fast = cv2.FastFeatureDetector_create(nonmaxSuppression=True,
+                threshold=100)
+        
+        # create BFMatcher object
+        self.bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+    
+
+    def determine_homography_to_reference_frame(self, frame):
+        pass
     
     def stabilize_frame(self, frame):
         if not self.has_seen_first_frame:
             cv2.imwrite("debug.png", frame)
-            self.extract_crops(frame)
+            self.first_frame = frame
+            self.kp_first_frame = self.fast.detect(frame, None)
+            self.kp_first_frame, self.des_first_frame = self.orb.compute(frame,
+                    self.kp_first_frame)
             self.has_seen_first_frame = True
 
-        self.locate_crop_matches(frame)
-        frame = self.correct_small_movements(frame)
-        return frame
+        kp = self.fast.detect(frame, None)
+        kp, des = self.orb.compute(frame, kp)
 
-    
-    def extract_crops(self, frame):
-        self.crops = []
-        for i, location in enumerate(self.crop_locations):
-            x_lower = location[1] - self.crop_height
-            x_upper = location[1] + self.crop_height
-            y_lower = location[0] - self.crop_width
-            y_upper = location[0] + self.crop_width
-            temp_frame = frame[x_lower:x_upper, y_lower:y_upper]
-            self.crops.append(temp_frame)
+        # Match descriptors.
+        matches = self.bf.match(des, self.des_first_frame)
 
+        # Sort them in the order of their distance.
+        matches = sorted(matches, key = lambda x:x.distance)
 
-    def locate_crop_matches(self, frame):
-        searchsize = 150
-        self.crop_matches = []
-        for i, crop in enumerate(self.crops):
-            match = self.locate_window(frame, crop, self.crop_locations[i],
-                    searchsize)
-            self.crop_matches.append(match)
+        good_matches = matches[:100]
+        good_matches = matches
 
+        src_pts = np.float32([ kp[m.queryIdx].pt for m in good_matches ]).reshape(-1,1,2)
+        dst_pts = np.float32([ self.kp_first_frame[m.trainIdx].pt for m in good_matches ]).reshape(-1,1,2)
 
-    def locate_window(self, frame, window, estimated_position, search_range):
-        # Remember to switch x and y when subsetting an image
-        cropped_frame = frame[estimated_position[1] - search_range:estimated_position[1] + search_range,
-                              estimated_position[0] - search_range:estimated_position[0] + search_range]
-        result = cv2.matchTemplate(cropped_frame, window, cv2.TM_SQDIFF)
-        _, _, match, _ = cv2.minMaxLoc(result, None)
-        match_adjusted = (estimated_position[0] - search_range + match[0] + window.shape[0] / 2,
-                          estimated_position[1] - search_range + match[1] + window.shape[1] / 2)
-        return match_adjusted
+        M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
+        # M, mask = cv2.findHomography(src_pts, dst_pts, cv2.LMEDS, 1.0)
 
-
-    def correct_small_movements(self, frame):
-        #for match in self.crop_matches:
-        #    cv2.circle(frame, match, 50, (255, 255, 0), 10)
-
-        pts1 = np.float32(self.crop_matches)
-        pts2 = np.float32(self.crop_locations)
-        M = cv2.getPerspectiveTransform(pts1, pts2)
         frame = cv2.warpPerspective(frame, M, dsize=(frame.shape[1],
             frame.shape[0]))
+
         return frame
 
 
@@ -85,8 +71,7 @@ class image_converter:
     self.bridge = CvBridge()
     self.image_sub = rospy.Subscriber("image_raw", Image, self.callback)
 
-    self.stabilizer = VideoStabilizer([[409, 250], [300, 762], [1123, 848],
-        [1600, 238]])
+    self.stabilizer = VideoStabilizer()
 
 
   def callback(self, data):
